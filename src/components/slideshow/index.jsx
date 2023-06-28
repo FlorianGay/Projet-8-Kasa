1c9a4a2fb399ac8588a422bc5e076ce09c3de3d3
Florian Gay <floriangay750@gmail.com>
2023-06-28 14:07:13 +0200

    Ajout d'un +1 pour le slideshow

--- src/components/slideshow/index.jsx
+++ src/components/slideshow/index.jsx
@@ -17,7 +17,7 @@ function Slideshow(props) {
             <img src={arrowLeft} alt="Flèche gauche" className="arrow arrow_left" onClick={slideLeft}/>
             <img src={arrowRight} alt="Flèche droite" className="arrow arrow_right" onClick={slideRight}/>
             <img src={`${props.img[currentIndex]}`} alt="Carrousel des photos du logement" className="slideshow_img" />
-            <span className="slideshow_img_position">{currentIndex}/{props.length}</span>
+            <span className="slideshow_img_position">{1+currentIndex}/{props.length}</span>
         </div>
         
             
